2f793f63774b00eddf476fae7c9be7b5f61087e8
Sean Sparkman <sean.sparkman@outlook.com>
2014-07-09 15:44:21 -0500

    Step 11 Xaml Created

---
+++ DFWMobile.XF/DFWMobile.XF/ListXaml.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFWMobile.XF
+{
+    public partial class ListXaml
+    {
+        public ListXaml()
+        {
+            InitializeComponent();
+        }
+    }
+}
