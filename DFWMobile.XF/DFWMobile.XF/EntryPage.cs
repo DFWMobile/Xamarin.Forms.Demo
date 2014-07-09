5f0a5fcd1977c4015078a91cb45fc6647a18b57c
Sean Sparkman <sean.sparkman@outlook.com>
2014-07-09 15:35:41 -0500

    Step 9 Entry Page

---
+++ DFWMobile.XF/DFWMobile.XF/EntryPage.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DFWMobile.XF
+{
+    public class EntryPage
+        : ContentPage
+    {
+        private Entry _firstName;
+        private Entry _lastName;
+        private Entry _twitter;
+        public EntryPage()
+        {
+            Title = "Data Entry";
+            Icon = "about.png";
+            Padding = 20;
+
+
+            var layout = new StackLayout
+            {
+                Orientation = StackOrientation.Vertical,
+                Spacing = 10
+            };
+
+            layout.Children.Add(new Label
+            {
+                Text = "First Name"
+            });
+
+            _firstName = new Entry
+            {
+                WidthRequest = 300,
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+            layout.Children.Add(_firstName);
+
+            layout.Children.Add(new Label
+            {
+                Text = "Last Name"
+            });
+
+            _lastName = new Entry
+            {
+                WidthRequest = 300,
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+            layout.Children.Add(_lastName);
+
+            layout.Children.Add(new Label
+            {
+                Text = "Twitter"
+            });
+
+            _twitter = new Entry
+            {
+                WidthRequest = 300,
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+            layout.Children.Add(_twitter);
+
+            Content = layout;
+        }
+    }
+}
